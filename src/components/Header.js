992c6234d4b9c9128d57cb063e4f2da9ea2ed309
Gayathri <gayathripsalian2001@gmail.com>
2024-02-23 17:28:27 +0530

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,14 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header">
-      <div className="header__logo">Amazon</div>
+      <div className="header__logo"></div>
+      <div className="header__location">
+      <div className="deliver">Deliver to</div>
+      <div className="location__container">
+        <span className="location__icon"></span>
+        <span className="location__text">India</span>
+      </div>
+    </div>
       <div className="header__search">
         <input type="text" placeholder="Search" />
         <button>Search</button>
